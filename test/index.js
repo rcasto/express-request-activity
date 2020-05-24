05324661891a588c6c2dc5ff8315b2f2d192b214
Richie Casto <rcasto92@gmail.com>
2020-05-24 16:30:42 -0700

    adjust test client to use new export structure

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const requestActivity = require('../index').requestActivity;
+const requestActivity = require('../index');
 
 const app = express();
 const port = process.env.PORT || 3000;
