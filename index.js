9340d099bd918aaf1b68609e8e008abb53503974
Richie Casto <rcasto92@gmail.com>
2020-05-24 15:43:14 -0700

    adding test client and documentation improvements

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const rpio = require('rpio');
 
 /**
  * requestActivity express middleware
+ * 
+ * Whenever a request is received, the configured pin output
+ * will be set high/on, turning the light on. 
+ * 
+ * It will then wait for the configured time
+ * and set the configured pin output to low/off to
+ * turn the light off.
+ * 
+ * If the light was already on when receiving a new request
+ * the on light timer will reset and wait the configured time
+ * again before turning the light off.
+ * 
  * @param {Object} config
- * @param {number} config.ledPin The GPIO pin the LED is connected to
+ * @param {number} config.lightPin The GPIO pin the light is connected to, this should be the physical pin number
+ * @param {number} config.lightOnTimeInMs The number of milliseconds the light should be kept on when activity is received
  * @returns {(req: Object, res: Object, next: () => void) => void}
  */
 function requestActivity(config) {
     let requestDebounceTimeoutId = null;
 
-    rpio.open(config.ledPin, rpio.OUTPUT);
+    rpio.open(config.lightPin, rpio.OUTPUT);
 
     /**
      * 
@@ -20,11 +33,11 @@ function requestActivity(config) {
     const _requestActivity = (req, res, next) => {
         clearTimeout(requestDebounceTimeoutId);
 
-        rpio.write(config.ledPin, rpio.HIGH);
+        rpio.write(config.lightPin, rpio.HIGH);
 
         requestDebounceTimeoutId = setTimeout(() => {
-            rpio.write(config.ledPin, rpio.LOW);
-        }, 100);
+            rpio.write(config.lightPin, rpio.LOW);
+        }, config.lightOnTimeInMs);
 
         next();
     };
---
+++ test/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const requestActivity = require('../index').requestActivity;
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(requestActivity({
+    lightPin: 11,
+    lightOnTimeInMs: 1000
+}));
+
+app.get('/', (req, res) => {
+    res.write('Hello World!');
+    res.end();
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
