02ac8a4cdfbd651f8c01ec17afc4bfbde175d480
Richie Casto <rcasto92@gmail.com>
2020-05-24 15:18:16 -0700

    Think thats it...time to play with it

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function requestActivity(req, res, next) {
-    // rpio stuff here
-    next();
+const rpio = require('rpio');
+
+/**
+ * requestActivity express middleware
+ * @param {Object} config
+ * @param {number} config.ledPin
+ */
+function requestActivity(config) {
+    let requestDebounceTimeoutId = null;
+
+    rpio.open(config.ledPin, rpio.OUTPUT);
+
+    return (req, res, next) => {
+        clearTimeout(requestDebounceTimeoutId);
+
+        rpio.write(config.ledPin, rpio.HIGH);
+
+        requestDebounceTimeoutId = setTimeout(() => {
+            rpio.write(config.ledPin, rpio.LOW);
+        }, 100);
+
+        next();
+    };
 }
 
 module.exports = {
