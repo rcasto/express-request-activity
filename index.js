140cad9bbfdb48834b7523ff782fc3447880b5a5
Richie Casto <rcasto92@gmail.com>
2020-05-24 15:27:53 -0700

    attempting documentation improvements

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const rpio = require('rpio');
 /**
  * requestActivity express middleware
  * @param {Object} config
- * @param {number} config.ledPin
+ * @param {number} config.ledPin The GPIO pin the LED is connected to
+ * @returns {(req: Object, res: Object, next: () => void) => void}
  */
 function requestActivity(config) {
     let requestDebounceTimeoutId = null;
 
     rpio.open(config.ledPin, rpio.OUTPUT);
 
-    return (req, res, next) => {
+    /**
+     * 
+     * @param {Object} req Express Request Object
+     * @param {Object} res Express Response Object
+     * @param {Function} next Express next function
+     */
+    const _requestActivity = (req, res, next) => {
         clearTimeout(requestDebounceTimeoutId);
 
         rpio.write(config.ledPin, rpio.HIGH);
@@ -21,6 +28,8 @@ function requestActivity(config) {
 
         next();
     };
+
+    return _requestActivity;
 }
 
 module.exports = {
